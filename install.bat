f98f320be73b90c61c0e7241064b5b02e93495d6
Sordeal-hash <goraibgoraib@gmail.com>
2024-06-16 17:41:58 +0300

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,9 +1,3 @@
-pip install requests
-pip install pycryptodome
-pip install pystyle
-pip install colorama
-pip install auto_py_to_exe
-pip install pyinstaller
-pip install wmi
-pip install customtkinter
-pip install cryptography
+@echo off
+pip install -r requirements.txt
+pause
